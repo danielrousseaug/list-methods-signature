9f94260f5780f46bfd17eaa250a0f5a774cf2285
Daniel Rousseau <danielrousseau@Daniels-MBP-2.attlocal.net>
2022-12-01 10:40:12 -0800

    fix

--- ListExamples.java
+++ ListExamples.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.*;
 
 interface StringChecker { boolean checkString(String s); }
 
@@ -8,7 +9,7 @@ class ListExamples {
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
-  static List<String> filter(StringChecker sc, List<String> list) {
+  static List<String> filter(List<String> list, StringChecker sc ) {
     List<String> result = new ArrayList<>();
     for(String s: list) {
       if(sc.checkString(s)) {
